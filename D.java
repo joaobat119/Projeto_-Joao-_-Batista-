065c4244ab7a085554e299096823a3ed8c0adb77
Joao Batista Miguel <joaobatistamigueljoaobatista@gmail.com>
2024-04-09 23:00:26 -0300

    Criada a Classe D com atributos e métodos

---
+++ D.java
@@ -0,0 +1,20 @@
+// Classe D
+public class D {
+    private String D1;
+    private int D2;
+
+    // Construtor
+    public D(String D1, int D2) {
+        this.D1 = D1;
+        this.D2 = D2;
+    }
+
+    // Métodos
+    public void MD1() {
+        System.out.println("Método MD1() da classe D");
+    }
+
+    public void MD2() {
+        System.out.println("Método MD2() da classe D");
+    }
+}

b60ce0fd4f1a3e73b108dea0ded6cb3b79f6707d
Joao Batista Miguel <joaobatistamigueljoaobatista@gmail.com>
2024-04-09 22:25:01 -0300

    Adicionado método MA3() à Classe A e criada Classe C

---
+++ C.java
@@ -0,0 +1,17 @@
+// Classe C
+public class C {
+    private String C1;
+    private int C2;
+
+    // Getters e Setters
+    // ...
+
+    // Métodos
+    public void MC1() {
+        System.out.println("Método MC1() da classe C");
+    }
+
+    public void MC2() {
+        System.out.println("Método MC2() da classe C");
+    }
+}
